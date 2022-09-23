668045b93bfb93f9738e68733f21358a1661a05c
Prince Nwaonicha <lolgfman@gmail.com>
2022-09-23 17:40:00 -0500

    #4 Created navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.scss';
 import {Routes, Route } from 'react-router-dom'
 import Home from './components/routes/Home/home.component';
-import { Outlet } from 'react-router-dom';
 
+import Navbar from './components/Navigation/navigation.component';
 import Hats from './components/routes/category/Hats/hats.component';
 import Jackets from './components/routes/category/Jackets/jackets.component';
 import Mens from './components/routes/category/Mens/mens.component';
@@ -10,22 +10,18 @@ import Sneakers from './components/routes/category/Sneakers/sneakers.component';
 import Womens from './components/routes/category/Womens/womens.component';
 
 const App = () => {
-  const Navbar = () => {
-    return (<>
-      <h1>I am Navbar</h1>
-      <Outlet />
-    </>)
-  }
+  
 
   return (
     <Routes>
       <Route path='/' element={<Navbar/>}>
-        <Route path='/' element={<Home />} />
+        <Route index element={<Home />} />
         <Route path='/Hats' element={<Hats />} />
         <Route path='/Jackets' element={<Jackets />} />
         <Route path='/Mens' element={<Mens />} />
         <Route path='/Sneakers' element={<Sneakers />} />
         <Route path='/Womens' element={<Womens />} />
+        <Route path='/shop'  />
       </Route>
     </Routes>
     
